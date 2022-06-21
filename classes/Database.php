c345550c9ae83112709c5588cc03269bce1720d5
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-06-21 18:43:57 +0200

    Changed name of Database

--- classes/Database.php
+++ classes/Database.php
@@ -1,18 +1,18 @@
-<?php 
-class Database {
+<?php
+class Database
+{
     private $host = "localhost";
     private $user = "root";
-    private $pass = "root";
-    private $db = "shop-db";
-    
+    private $pass = "";
+    private $db = "webshop-db";
+
     protected $conn;
-    
+
     public function __construct()
     {
-        $this->conn = mysqli_connect($this->host,$this->user,$this->pass, $this->db);
-        if(!$this->conn) {
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+        if (!$this->conn) {
             die("Error connecting to db.");
         }
     }
-
 }
