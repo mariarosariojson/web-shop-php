7d32e868d0f08f912076372fb0c39022f82d022f
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-06-28 13:49:57 +0200

    Orders databases created

---
+++ classes/OrdersDatabase.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . "/Database.php";
+require_once __DIR__ . "/Order.php";
+
+class OrdersDatabase extends Database
+{
+    // get_one
+
+
+    // get_all
+
+
+    // create
+
+
+    // update
+
+
+    // delete
+
+}
