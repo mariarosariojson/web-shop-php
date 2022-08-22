ee1e16737aacb2c29ecaf38e6f0435c365fbd259
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-08-22 11:54:56 +0200

    register page styling

--- pages/register.php
+++ pages/register.php
@@ -3,15 +3,39 @@ require_once __DIR__ . '/../classes/Template.php';
 
 Template::header("Register");
 ?>
-
 <div class="register-container">
-
     <form action="/ws/scripts/post-register-user.php" method="post">
-        <input type="text" name="username" placeholder="Username"><br>
-        <input type="password" name="password" placeholder="Password"><br>
-        <input type="password" name="confirm-password" placeholder="Confirm password"><br>
-        <input type="submit" value="Register">
+        <div class="row">
+            <div class="col-25">
+                <label for="username">Username</label>
+            </div>
+            <div class="col-75">
+                <input type="text" id="username" name="username" placeholder="Username..">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="password">Password</label>
+            </div>
+            <div class="col-75">
+                <input type="password" name="password" placeholder="Password.."><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="password">Confirm password</label>
+            </div>
+            <div class="col-75">
+                <input type="password" name="confirm-password" placeholder="Confirm password.."><br>
+                <input type="submit" value="Register">
+
+            </div>
+        </div>
+
     </form>
+
+
+
 </div>
 <br>
 
