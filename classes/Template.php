6f8d6be522fb0750f166657650f3da15231ecd28
Maria Rosario <mariarosariojohansson@gmail.com>
2022-08-27 17:46:01 +0200

    pictures updated

--- classes/Template.php
+++ classes/Template.php
@@ -40,7 +40,6 @@ class Template
                     <h1><?= $title ?></h1>
                 </div>
                 <nav class="navbar">
-                    <a href="/ws/pages/cart.php"><i class="fa fa-shopping-cart"></i> (<?= $cart_count ?>)</a>
                     <a href="/ws/index.php">Home</a>
                     <a href="/ws/pages/products.php">Shop</a>
 
@@ -48,6 +47,7 @@ class Template
                         <a href="/ws/pages/orders.php">My Orders </a>
                     <?php endif; ?>
 
+                    <a href="/ws/pages/cart.php"><i class="fa fa-shopping-cart"></i> (<?= $cart_count ?>)</a>
 
                     <?php if (!$is_logged_in) : ?>
                         <a href="/ws/pages/login.php">Log in</a>
